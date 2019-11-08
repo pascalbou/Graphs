b018d1209e4521eb9b218671a14393e816112ecf
krikaz <pascal.bou@protonmail.com>
2019-11-08 10:04:41 +0100

    picks the exit with only one room first

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -19,7 +19,7 @@ roomGraph={494: [(1, 8), {'e': 457}], 492: [(1, 20), {'e': 400}], 493: [(2, 5), 
 world.loadGraph(roomGraph)
 
 # UNCOMMENT TO VIEW MAP
-# world.printRooms()
+world.printRooms()
 
 player = Player("Pascal", world.startingRoom)
 
@@ -40,7 +40,24 @@ def get_opposite(direction):
         return 'e'   
 
 def get_random_exit(exits):
-    return random.choice(exits)                     
+    return random.choice(exits)
+
+def get_shortest_exit(exits, room):
+    shortest = []
+
+    for ex in exits:
+        room_in_direction = room.getRoomInDirection(ex)
+        # print(room_in_direction)
+        len_exits = len(room_in_direction.getExits())
+        # print(len_exits)
+        if len_exits == 1:
+            shortest.append(ex)
+
+    print(shortest)
+    if len(shortest) != 0:
+        return random.choice(shortest)   
+    else:
+        return None                  
 
 def game(player):
     result = []
@@ -58,7 +75,7 @@ def game(player):
     # print(stack.size())
     while stack.size() and len(graph.vertices) != len(roomGraph):
         # print(current.id)
-        # time.sleep(.100)
+        # time.sleep(1)
 
         exits = current.getExits()
         # print(current.getExits())
@@ -84,7 +101,8 @@ def game(player):
             stack.push(current.id)
             unknown_exits = []
         else:
-            direction = get_random_exit(unknown_exits)
+            # direction = get_random_exit(unknown_exits)
+            direction = get_shortest_exit(unknown_exits, current) or get_random_exit(unknown_exits)
             way_back.push(get_opposite(direction))   
 
             if graph.vertices[current_vertex][direction] == '?':
